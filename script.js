83c27e473bec124f24e0877de4481f6205670102
Matheus Rodrigues <mmrodrigues077@gmail.com>
2023-08-24 14:19:39 -0300

    Portfolio finalizado

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
-const toggleMenuOpen = () => document.body.classList.toggle("open");
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header")
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
 
-
-
-/*=============================  scroll section navbar ======================*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onClick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+function toggleMenu() {
+    const menuToggle = document.querySelector(".menuToggle")
+    const navigation = document.querySelector(".navigation")
+    menuToggle.classList.toggle("active")
+    navigation.classList.toggle("active")
 }
-
-/*============================= scrool navbar ======================*/
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
-        };
-    })
-
-    /*============================= stick navbar ======================*/
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-};
 
 
 
